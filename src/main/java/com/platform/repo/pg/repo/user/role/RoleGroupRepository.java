fdf2eac679162bf911897b63417dcb3bccb585ea
Advance <yanl.fu@outlook.com>
2021-10-08 18:40:49 +0800

    修改存储过程入参

--- src/main/java/com/platform/repo/pg/repo/user/role/RoleGroupRepository.java
+++ src/main/java/com/platform/repo/pg/repo/user/role/RoleGroupRepository.java
@@ -3,6 +3,7 @@ package com.platform.repo.pg.repo.user.role;
 import com.platform.repo.pg.model.base.BasePlpgsqlModel;
 import com.platform.repo.pg.repo.base.CommonCustomRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * 角色组
@@ -17,8 +18,16 @@ public interface RoleGroupRepository extends CommonCustomRepository<BasePlpgsqlM
      * @param in_rolegrpid
      * @return
      */
-    @Query(value= "select * from aix.f_mb_role_group_query(?1)", nativeQuery = true)
-    BasePlpgsqlModel queryDxRoleGroup(String in_rolegrpid);
+    @Query(value= "select * from aix.f_mb_role_group_query(" +
+            ":in_rolegrpid, " +
+            ":in_countflag, " +
+            ":in_offset, " +
+            ":in_pagesize " +
+            ")", nativeQuery = true)
+    BasePlpgsqlModel queryDxRoleGroup(@Param("in_rolegrpid")String in_rolegrpid,
+                                      @Param("in_countflag")Integer in_countflag,
+                                      @Param("in_offset")Integer in_offset,
+                                      @Param("in_pagesize")Integer in_pagesize);
 
     /**
      * 新增角色
